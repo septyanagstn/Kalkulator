15f9d20fdda0f00a6d6ae88ee9e1431c9352a04d
niqanabila16 <niqanabila16@gmail.com>
2025-05-04 16:51:56 +0700

    penambahan test validasi null angka dan operator

--- app/src/test/java/kalkulatorapp/KomputasiTest.java
+++ app/src/test/java/kalkulatorapp/KomputasiTest.java
@@ -40,8 +40,24 @@ public class KomputasiTest {
         Komputasi.hitung("2", "3", "%");
     }
 
-    @Test(expected = ArithmeticException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testPembagiNol() {
         Komputasi.hitung("2", "0", "/");
     }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testInputANull() {
+        Komputasi.hitung(null, "2", "+");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInputBNull() {
+        Komputasi.hitung("2", null, "+");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOperatorNull() {
+        Komputasi.hitung("2", "3", null);
+    }
+
 }
